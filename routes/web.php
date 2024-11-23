dd8b5fd7f443c8a743d9b14c173605baf80e472a
Sam Watts <samuel.watts67@gmail.com>
2024-11-23 15:59:39 +0000

    Modify routes and fix failing tests

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
 
-use App\Facades\Quote;
 use Illuminate\Support\Facades\Route;
-use App\Services\QuoteService;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/', function () {
+        return view('quotes');
+    })->name('quotes');
 });
