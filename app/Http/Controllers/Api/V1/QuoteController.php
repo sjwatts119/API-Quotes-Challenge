308e0d7094fdaf4278f4f2757c8dfa172cf159d2
Sam Watts <samuel.watts67@gmail.com>
2024-11-23 15:14:06 +0000

    Install telescope and API resources

---
+++ app/Http/Controllers/Api/V1/QuoteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Facades\Quote;
+use App\Http\Resources\QuoteResource;
+
+class QuoteController
+{
+    public function index(): QuoteResource
+    {
+        return new QuoteResource(Quote::getQuotes(5));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\V1\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')
+    ->name('v1.')
+    ->group(function () {
+        Route::name('quotes.')
+            ->controller(QuoteController::class)
+            ->group(function () {
+                Route::get('/quotes', 'index')->name('index');
+            });
+    });
